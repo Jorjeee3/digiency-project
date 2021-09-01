e9851cf07600356cab978ba9ad9c147232cdaf95
Jorjeee3 <look7at7george@gmail.com>
2021-09-01 12:03:47 +0300

    feedback section

--- src/js/script.js
+++ src/js/script.js
@@ -105,6 +105,33 @@ $(document).ready(function () {
 	});
 })
 
+$(document).ready(function () {
+	$('.feedback-slider-list').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		buttons: false,
+		prevArrow: false,
+		nextArrow: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 650,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+})
+
 
 
 
