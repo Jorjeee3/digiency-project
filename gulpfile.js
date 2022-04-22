a0fc9c78e69c61cf0868708d885c9f220a1ce43e
Jorjeee3 <look7at7george@gmail.com>
2022-04-22 23:53:25 +0300

    fix icons & footer

--- gulpfile.js
+++ gulpfile.js
@@ -5,52 +5,48 @@ const cssnano = require('cssnano');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 let gulp = require('gulp'); 
-let svgSprite = require('gulp-svg-sprite');
+// let svgSprite = require('gulp-svg-sprite');
 
 // Sass Task
 function scssTask(){
   return src('src/scss/style.scss', { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano()]))
-    .pipe(dest('dist', { sourcemaps: '.' }));
+    .pipe(dest('dist', { sourcemaps: '.' }))
 }
 
 // JavaScript Task
 function jsTask(){
   return src('src/js/script.js', { sourcemaps: true })
     .pipe(terser())
-    .pipe(dest('dist', { sourcemaps: '.' }));
+    .pipe(dest('dist', { sourcemaps: '.' }))
 }
 
-// Browsersync Tasks
+
 function browsersyncServe(cb){
   browsersync.init({
     server: {
       baseDir: '.'
-    }
+    }    
   });
   cb();
 }
 
-async function browsersyncReload(cb) {
-  await gulp.src('./static/icons/*.svg')
-    .pipe(svgSprite({
-      mode: {
-        stack: {
-          sprite: "./sprite.svg"
-        }
-      }
-    }))
-    .pipe(gulp.dest('./static/sprite'));
-  
+
+function browsersyncReload() {
   browsersync.reload();
   cb();
 }
 
+
 // Watch Task
 function watchTask(){
-  watch('*.html', browsersyncReload);
-  watch(['src/scss/**/*.scss','src/styleComponents/**/*.scss', 'src/js/**/*.js', 'static/icons/*.svg'], series(scssTask, jsTask, browsersyncReload));
+  watch('*.html', browsersyncReload)
+  watch(['src/scss/**/*.scss', 'src/js/**/*.js', 'src/styleComponents/**/*.scss'], series(scssTask, jsTask, browsersyncReload))
+  // gulp.watch('*.html', browsersync.reload);
+  // gulp.watch('*.dist/style.css').on('change', browsersync.reload) 
+  // gulp.watch('*.dist/style.css', browsersync.reload);
+  // gulp.watch(['src/scss/**/*.scss','src/styleComponents/**/*.scss', 'src/js/**/*.js', 'static/icons/*.svg'], series(scssTask, jsTask, browsersync.reload));
 }
 
 // Default Gulp task
@@ -61,36 +57,3 @@ exports.default = series(
   watchTask
 );
 
-// gulp.task('svg-sprite', function () {
-// return gulp.src('./static/icons/*.svg')
-//     .pipe(svgSprite({
-//       mode: {
-//         stack: {
-//           sprite: "./sprite.svg"
-//         }
-//       }
-//     }))
-//     .pipe(gulp.dest('./static/sprite'));
-// }); 
-
-config = {
-    shape: {
-      dimension: { // Set maximum dimensions
-        maxWidth: 32,
-        maxHeight: 32
-      },
-      spacing: { // Add padding
-        padding: 10
-      },
-      dest: 'static/sprite/intermediate-svg' // Keep the intermediate files
-    },
-    mode: {
-      view: { 
-        bust: false,
-        render: {
-          scss: true 
-        }
-      },
-      symbol: true 
-    }
-};
